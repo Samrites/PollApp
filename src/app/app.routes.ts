eba961a3314338a75da85cec464c83e238b1ae01
samrawit <samrites2008@yahoo.com>
2026-08-22 13:06:07 +0200

    document and format application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,36 @@
+
 import { Routes } from '@angular/router';
+
 import { HomePage } from './pages/home/home-page';
 import { SingleSurveyPage } from './pages/single-survey/single-survey-page';
 
+/**
+ * Defines the application's routes.
+ *
+ * Includes routes for the home page, survey details,
+ * redirects, and unknown URLs.
+ */
 export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'create-survey', redirectTo: '', pathMatch: 'full' },
-  { path: 'single-survey', redirectTo: 'single-survey/1', pathMatch: 'full' },
-  { path: 'single-survey/:id', component: SingleSurveyPage },
-  { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'create-survey',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'single-survey',
+    redirectTo: 'single-survey/1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'single-survey/:id',
+    component: SingleSurveyPage,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
